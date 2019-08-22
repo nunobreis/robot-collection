f26284bc3af7d34c17ef33bce64eff21fc772936
Nuno <nuno@Nunos-MacBook-Pro.local>
2019-08-22 17:24:38 +0100

    improves performance

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Card = ({ name, id, email, ...props }) => (
   <div className="bg-light-green dib br3 pa3 ma2 grow">
-    <img src={`https://robohash.org/${id}?200x200`} alt="robots" />
+    <img src={`https://robohash.org/${id}?size=200x200`} alt="robots" />
     <div>
       <h2>{name}</h2>
       <p>{email}</p>
--- src/Components/SearchBox.js
+++ src/Components/SearchBox.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 const SearchBox = ({ searchfield, searchChange }) => <div className="pa2">
   <input
+    aria-label="Search Robots"
     className="pa3 ba b--green bg-lightest-blue"
     type="text"
     placeholder="search for a robot"
