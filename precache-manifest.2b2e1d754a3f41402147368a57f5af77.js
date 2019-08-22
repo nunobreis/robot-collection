441125a6cb8f2d7ba1df31ce2a561c720777090b
Nuno <nuno@Nunos-MacBook-Pro.local>
2019-08-22 17:05:39 +0100

    Updates

---
+++ precache-manifest.2b2e1d754a3f41402147368a57f5af77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08e641cbddabce45c5639fc1cd9a9824",
+    "url": "/robot-collection/index.html"
+  },
+  {
+    "revision": "aba180414206be2792fb",
+    "url": "/robot-collection/static/css/2.e40df323.chunk.css"
+  },
+  {
+    "revision": "91d828f4769b9c814648",
+    "url": "/robot-collection/static/css/main.b52c66d6.chunk.css"
+  },
+  {
+    "revision": "aba180414206be2792fb",
+    "url": "/robot-collection/static/js/2.5c29fc69.chunk.js"
+  },
+  {
+    "revision": "91d828f4769b9c814648",
+    "url": "/robot-collection/static/js/main.36ca12e4.chunk.js"
+  },
+  {
+    "revision": "01983e7c82097a1efc30",
+    "url": "/robot-collection/static/js/runtime~main.44ecf1e2.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robot-collection/static/media/SEGA.12a3db8b.woff"
+  }
+]);
